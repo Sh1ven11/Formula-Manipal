1a36c50db992fdcf5d2b173bfc70be467053ed93
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-02-13 22:46:28 +0530

    logo

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,5 @@
 // Function to change menu color based on section in view
+var open=0;
 function updateMenuColor() {
     const menuIconLines = document.querySelectorAll(".menu .line");
     const homepage = document.querySelector(".homepage");
@@ -9,7 +10,7 @@ function updateMenuColor() {
     console.log(homepageRect.top +"bottom " +homepageRect.bottom)
     if (homepageRect.top <= 0 && homepageRect.bottom >= 0) {
         menuIconLines.forEach(line => line.style.stroke = "white");  // White in Homepage
-    } else if (aboutUsRect.top <= 0 && aboutUsRect.bottom >= 0) {
+    } else if (aboutUsRect.top <= 0 && aboutUsRect.bottom >= 0 && open==1) {
         menuIconLines.forEach(line => line.style.stroke = "black");  // Black in ABOUT US
     }
 }
@@ -20,16 +21,18 @@ document.querySelector(".menu").addEventListener("click",function(){
   
 if (element.style.display === "block") {
     element.style.display = "none";  
+    open=1;
     document.querySelector(".sidebar").classList.remove("open")
-
+    updateMenuColor();
 } 
 else{
     document.querySelector(".sidebar").classList.add("open")
-
+    open=0;
     element.style.display = "block";   
 
     const menuIconLines = document.querySelectorAll(".menu .line");
     menuIconLines.forEach(line => line.style.stroke = "white");
+
 
 }
 
