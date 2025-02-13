d089ea04aae29b8a535924ab7b528384a93a35e3
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-02-13 17:39:04 +0530

    1

--- JS/app.js
+++ JS/app.js
@@ -7,7 +7,7 @@ function updateMenuColor() {
     const homepageRect = homepage.getBoundingClientRect();
     const aboutUsRect = aboutUs.getBoundingClientRect();
     console.log(homepageRect.top +"bottom " +homepageRect.bottom)
-    if (homepageRect.top <= 0 && homepageRect.bottom >= 0) {
+    if (homepageRect.top <= 0 && homepageRect.bottom >= 50) {
         menuIconLines.forEach(line => line.style.stroke = "white");  // White in Homepage
     } else if (aboutUsRect.top <= 0 && aboutUsRect.bottom >= 0) {
         menuIconLines.forEach(line => line.style.stroke = "black");  // Black in ABOUT US
