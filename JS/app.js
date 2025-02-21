4ec4d302db9df6093e7a2cf07257df08978de3ab
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-02-21 20:33:39 +0530

    new section added

--- JS/app.js
+++ JS/app.js
@@ -4,14 +4,19 @@ function updateMenuColor() {
     const menuIconLines = document.querySelectorAll(".menu .line");
     const homepage = document.querySelector(".homepage");
     const aboutUs = document.querySelector(".ABOUTUS");
+    const sponsor =document.querySelector(".Sponsors")
 
+    const sponsorRect=sponsor.getBoundingClientRect();
     const homepageRect = homepage.getBoundingClientRect();
     const aboutUsRect = aboutUs.getBoundingClientRect();
-    console.log(homepageRect.top +"bottom " +homepageRect.bottom +" about su"+aboutUsRect.top+"bottom "+aboutUsRect.bottom)
+
+    console.log(" about su"+aboutUsRect.top+"bottom "+aboutUsRect.bottom+"Sponsor"+sponsorRect.top+"bottom"+sponsorRect.bottom)
     if (homepageRect.top <= 0 && homepageRect.bottom >= 40 && open%2==0) {
         menuIconLines.forEach(line => line.style.stroke = "white");  // White in Homepage
-    } else if (aboutUsRect.top <= 40 && aboutUsRect.bottom >= 0 ) {
+    } else if (aboutUsRect.top <= 40 && aboutUsRect.bottom >= 10 ) {
         menuIconLines.forEach(line => line.style.stroke = "black");  // Black in ABOUT US
+    }else{
+        menuIconLines.forEach(line => line.style.stroke="white")
     }
 }
 document.querySelector(".menu").addEventListener("click",function(){
